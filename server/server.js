c0f4a6c48e09fd0a9923cf1710c57026a8292b4c
yczar <babalolagbogo@gmail.com>
2022-01-17 23:42:20 +0100

    Add Get Todos ID

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectId } = require('mongodb');
 
 const req = require('express/lib/request');
 var { mongoose } = require('./db/mongoose');
@@ -17,6 +18,31 @@ app.post('/todos', (req, res) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
